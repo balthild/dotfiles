ab25f18e334d1417c29847e681c1aeb95650c152
Balthild <ibalthild@gmail.com>
2025-03-15 19:06:31 -0500

    Update

--- nix/flake.nix
+++ nix/flake.nix
@@ -8,14 +8,11 @@
   };
 
   outputs = inputs@{ self, darwin, nixpkgs }: {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .
-
     # Configurations per system. The name should match the hostname
     darwinConfigurations = {
       "Balthild-Mac" = darwin.lib.darwinSystem {
         modules = [ ./darwin.nix ];
-      }
+      };
     };
   };
 }
