7c71bc1056aa55e1aa03183e9ca29ae8a453a353
Balthild <ibalthild@gmail.com>
2025-05-06 15:10:12 -0500

    Update

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,3 +1,11 @@
+# Not using `conf.d` to prevent them being loaded by $__fish_data_dir/config.fish
+# and thus causing infinite recursion in carapace.
+# https://github.com/carapace-sh/carapace-bin/issues/2781
+for file in $__fish_config_dir/profiles/*.fish
+  test -f $file -a -r $file
+  and source $file
+end
+
 set fish_pager_color_description '-d'
 
 set -gx QUOTING_STYLE 'literal'
