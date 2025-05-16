6dad53cf71657250f1a9efd77d1d6a595eacad39
Balthild <ibalthild@gmail.com>
2025-05-16 15:32:38 -0500

    Update

---
+++ nix/overlays/carapace.nix
@@ -0,0 +1,8 @@
+{ prev }:
+
+prev.carapace.overrideAttrs (old: {
+  postPatch = (old.postPatch or "") + ''
+    rm completers/brew_completer/cmd/instal.go
+    rm completers/brew_completer/cmd/uninstal.go
+  '';
+})
--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -2,4 +2,5 @@
 
 final: prev: {
   hyfetch-rs = prev.callPackage ./hyfetch-rs.nix { };
+  carapace = prev.callPackage ./carapace.nix { inherit prev; };
 }
