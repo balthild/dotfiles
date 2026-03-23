10eebef01990b21d8a818a7433bc40fcabcd1e74
Balthild <ibalthild@gmail.com>
2026-03-23 08:14:59 -0500

    Update

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,18 +2,17 @@
   description = "Example nix-darwin system flake";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/09061f748ee21f68a089cd5d91ec1859cd93d0be";
-    darwin.url = "github:LnL7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, darwin, home-manager }: {
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager }: {
     # Configurations per system. The name should match the hostname
     darwinConfigurations = {
-      "Balthild-Mac" = darwin.lib.darwinSystem {
+      "Balthild-Mac" = nix-darwin.lib.darwinSystem {
         modules = [
           ./darwin.nix
           home-manager.darwinModules.home-manager {
